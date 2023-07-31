2a7ba909a2a10b9a6dc6a7a92630e8debd8bc459
SilverFlin <toledorusso@outlook.com>
2023-07-30 18:21:54 -0700

    feat: home bg image

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,21 +1,24 @@
 <template>
-    <div class="flex items-center h-20 mx-6 bg-red-200">
-        <span class="mr-auto">
-            <Icon height="24" icon="logos:netflix" />
-        </span>
-        <span class="flex items-center justify-center w-16 h-8 mx-1 text-white border-[2px] border-[#767676] rounded-md">
+    <div class="absolute left-0 right-0 w-screen h-20 ">
+        <div class="flex items-center w-[90%] h-20 mx-auto">
+            <span class="mr-auto">
+                <Icon height="24" icon="logos:netflix" />
+            </span>
+            <span
+                class="bg-[rgba(48,46,46,60%)] flex items-center justify-center w-16 h-8 mx-1 text-white border-[2px] border-[#767676] rounded-md">
 
-            <Icon width="22" icon="jam:world" class="" />
-            <select class="w-1 bg-transparent">
-                <option>
-                    English
-                </option>
-            </select>
-            <Icon icon="octicon:triangle-down-16" />
-        </span>
-        <span class="ml-1">
-            <button class="bg-[#E50914] text-white font-bold px-4 py-1 rounded-md">Sign In</button>
-        </span>
+                <Icon width="22" icon="jam:world" class="" />
+                <select class="w-1 bg-transparent">
+                    <option>
+                        English
+                    </option>
+                </select>
+                <Icon icon="octicon:triangle-down-16" />
+            </span>
+            <span class="ml-1">
+                <button class="bg-[#E50914] text-white font-bold px-4 py-1 rounded-md">Sign In</button>
+            </span>
+        </div>
     </div>
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,21 @@
 <template>
-    <div>
-        a
+    <div style=" background-image:
+        url('https://assets.nflxext.com/ffe/siteui/vlv3/5eab1b22-c5ea-48b0-8ef4-862b3fa6df2c/3805d282-3a68-4c43-9395-d7a81bbd6980/MX-en-20230724-popsignuptwoweeks-perspective_alpha_website_small.jpg');"
+        class="w-full h-screen bg-center bg-no-repeat bg-cover">
+        <div
+            class="flex flex-col items-center justify-center w-full h-screen bg-gradient-to-b from-black via-transparent to-black">
+            <div class="px-3 text-center text-white">
+                <h1 class="text-3xl font-bold">Unlimited movies, TV shows, and more</h1>
+                <p class="mt-4">Watch anywhere. Cancel anytime.</p>
+                <div class="h-24 mt-6 ">
+                    <form class="flex flex-col justify-around">
+                        <h1>Ready to watch? Enter your email to create or restart your membership.</h1>
+                        <input type="email" name="" placeholder="Email address" id="">
+                    </form>
+                </div>
+            </div>
+
+        </div>
+
     </div>
 </template>
